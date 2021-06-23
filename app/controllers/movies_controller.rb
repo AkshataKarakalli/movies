4821b384193eb1614859948f88c0c6b58cdb8483
AkshataKarakalli <akshatakarakalli@gmail.com>
2021-06-23 19:45:31 +0530

    addedcolumn

--- app/controllers/movies_controller.rb
+++ app/controllers/movies_controller.rb
@@ -1,5 +1,8 @@
 class MoviesController < ApplicationController
+ #def index
+    #@movies= "ashique2,maghadhir,avatar2"
+ #end
  def index
-    @movies= "ashique2,maghadhir,avatar2"
+    @movies = Movie.all
  end
 end
